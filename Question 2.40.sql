ed853bdb7b3e2c0c6cecfd0cfc2566e816dc1899
Theophilus Danladi Umar <theophilusumar27@gmail.com>
2025-08-12 01:00:49 +0100

    Create Question 2.40.sql

---
+++ Question 2.40.sql
@@ -0,0 +1,8 @@
+
+select sku,sku_description, warehouseid,w
+
+
+
+Write an SQL statement to display the SKU, SKU_Description, WarehouseID, 
+WarehouseCity, and WarehouseState for all items stored in the Atlanta, Bangor, or 
+Chicago warehouse. Do not use the IN keyword.
