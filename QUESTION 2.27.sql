cb3a273bc8c6f9df8e657684ed552a0b9a3bb7f8
Theophilus Danladi Umar <theophilusumar27@gmail.com>
2025-08-12 01:00:26 +0100

    Update QUESTION 2.27.sql

--- QUESTION 2.27.sql
+++ QUESTION 2.27.sql
@@ -1,7 +1,6 @@
 SELECT SKU,SKU_DESCRIPTION,WAREHOUSEID
 FROM INVENTORY
-
-where QuantityOnHand = 0 and QuantityOnOrder >0
+where QuantityOnHand = 0 and QuantityOnOrder > 0
 order by warehouseid desc, sku asc;
 
 
