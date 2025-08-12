668b1cbeb89e640e78a2826aead695b3f641e4d8
Theophilus Danladi Umar <theophilusumar27@gmail.com>
2025-08-12 06:50:16 +0100

    Create Question G.sql

---
+++ MDC-CH02-CQ.sql/Question G.sql
@@ -0,0 +1,11 @@
+select firstname,lastname,phone
+
+from customer
+
+where 
+
+
+
+/*List the LastName, FirstName, and Phone for all customers whose second and third 
+digits (from the left) of their phone number are 23. For example, any phone number 
+with an area code of “723” would meet the criteria.*/
